d0c9313077483d0178186ea9427555f8b043f30a
beth nduta <bethnduta05@gmail.com>
2022-07-06 06:45:52 +0300

    created 4 components(quiz,register,navbar,result)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ResultComponent } from './result/result.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ResultComponent,
+    QuizComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
