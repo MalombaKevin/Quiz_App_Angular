a191ae0233867aac8dff316d02fc7b9636dbede4
Norah.Waswala <norah.waswala@student.moringa.com>
2022-07-06 06:43:37 +0300

    feat: create service classes for aunthentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EndpointsService } from './service/endpoints.service';
+import { AuthService } from 'src/app/service/auth.service';
+import { UnAuthGuardService } from './guards/unauth-guard.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/.component';
+import { RegisterComponent } from './authentication/register/register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { LoginComponent } from './authentication/login/login.component';
+import { GlobalService } from './service/global.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    CookieService,
+    AuthGuardService,
+    UnAuthGuardService,
+    AuthService,
+    EndpointsService,
+    GlobalService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
