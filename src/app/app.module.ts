03b31e7bf944250dcbf3fe7f7d5078f172e849a7
Michael Ogaye <hogaeymic@gmail.com>
2022-07-06 14:40:51 +0300

    pt:global service removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,19 @@
+import { GeneralService } from './services/general.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-import { EndpointsService } from './service/endpoints.service';
-import { AuthService } from 'src/app/service/auth.service';
-
-
-=======
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> michael
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
-
-
-import { HttpClientModule } from '@angular/common/http';
-
-
-import { GlobalService } from './service/global.service';
 
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CookieService } from 'ngx-cookie-service';
 import { RegisterComponent } from './authc/register/register.component';
+import { AuthService } from './services/auth.service';
+import { EndpointsService } from './services/endpoints.service';
 
 
 @NgModule({
@@ -52,7 +41,7 @@ import { RegisterComponent } from './authc/register/register.component';
 
     AuthService,
     EndpointsService,
-    GlobalService,
+    GeneralService,
   ],
 
   bootstrap: [AppComponent]
