661c719a8f62e636d5d3a3ee8e2acba7aff26558
Michael Ogaye <hogaeymic@gmail.com>
2022-07-10 02:26:00 +0300

    ft:display of quizzes and questions

--- src/app/quiz/quiz-detail/quiz-detail/question/answer/answer.component.ts
+++ src/app/quiz/quiz-detail/quiz-detail/question/answer/answer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input,Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-answer',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./answer.component.css']
 })
 export class AnswerComponent implements OnInit {
+  @Input()
+  answer: any;
 
+  @Input()
+  index!: number;
+
+  @Input()
+  selectedAnswer!: number;
+
+  letter!: string;
+
+  @Output()
+  selectAnswer = new EventEmitter<any>();
+    
+
+
+  callParent(id: number) {
+    this.selectAnswer.next(id);
+  }
+
+  isSelected() {
+    return this.selectedAnswer == this.answer.id;
+  }
   constructor() { }
 
   ngOnInit(): void {
+    this.letter = String.fromCharCode(65 + this.index);
+
   }
 
 }
